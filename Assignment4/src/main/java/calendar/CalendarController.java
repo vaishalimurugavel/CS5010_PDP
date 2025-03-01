6f41075290ad4e417714431e3c078ca8d6b99ab8
Vaish <vaishali.murugavel@gmail.com>
2025-03-01 14:59:01 -0500

    Files include

---
+++ Assignment4/src/main/java/calendar/CalendarController.java
@@ -0,0 +1,27 @@
+package calendar;
+
+import java.util.Scanner;
+
+public class CalendarController {
+
+  private void processCommand(String command) {
+
+    
+  }
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    String input;
+
+    do {
+      System.out.println("Enter Calendar command (or 'exit' to quit):");
+      input = scanner.nextLine();
+
+      if (!input.equalsIgnoreCase("exit")) {
+        System.out.println("Executing command: " + input);
+      }
+    } while (!input.equalsIgnoreCase("exit"));
+
+    System.out.println("Exiting application.");
+    scanner.close();
+  }
+}
---
+++ Assignment4/src/main/java/calendar/DateTime.java
@@ -0,0 +1,35 @@
+package calendar;
+
+public class DateTime {
+  private final int hours;
+  private final int minutes;
+  private final int seconds;
+  private final int day;
+  private final int month;
+  private final int year;
+
+  public DateTime(int hours, int minutes, int seconds, int day, int month, int year) throws IllegalArgumentException  {
+
+    if ((hours < 0) || (minutes < 0) || (seconds < 0)) {
+      throw new IllegalArgumentException("Negative durations are not supported");
+    }
+    if ((day < 0) ||  (month < 0) || (year < 0)) {
+      throw new IllegalArgumentException("Negative date/month/year are not supported");
+    }
+
+    this.hours = hours;
+    this.minutes = minutes;
+    this.seconds = seconds;
+    this.day = day;
+    this.month = month;
+    this.year = year;
+  }
+
+  public String getTime() {
+    return String.format("%d:%02d:%02d", hours, minutes, seconds);
+  }
+
+  public String getDate(){
+    return String.format("%02d/%02d/%02d", month, day, year);
+  }
+}
